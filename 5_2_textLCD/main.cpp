b6b80c73a3c23446925a32c346ced33bcd37cb1c
siriusasd <shawn1.2311@gmail.com>
2021-03-22 16:08:05 +0800

    add example 2

--- 5_2_textLCD/main.cpp
+++ 5_2_textLCD/main.cpp
@@ -1,13 +1,19 @@
 #include "mbed.h"
 #include "TextLCD.h"
 
-TextLCD lcd(D2, D3, D4, D5, D6, D7); // RS, E, DB4-DB7
+DigitalOut led(LED1);      //LED1 = PA_5
+TextLCD lcd(D2, D3, D4, D5, D6, D7);
 
 int main()
 {
-      lcd.printf("HELLO\n");
-      for (char x = 0x00; x <= 0x09; x++)
-      { // display numbers 0-9
-            lcd.printf("%u", x);
+      int x=0;
+      lcd.printf("Hello World!\n");
+      while(true)
+      {
+            led = !led;             // toggle led
+            lcd.locate(5,1);
+            lcd.printf("%5i",x);    //counter display
+            ThisThread::sleep_for(1s);
+            x++;
       }
 }
